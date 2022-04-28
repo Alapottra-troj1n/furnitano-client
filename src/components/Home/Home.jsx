139d853d3dfa4be3169a94e7b1a21d2d389586c2
Alapottra <alapottratrojon@gmail.com>
2022-04-28 23:08:06 +0600

    added button and transition effect

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
      <div className="_hero-section h-screen w-full flex justify-center items-center flex-col">
             <p className="text-sm lg:text-2xl font-medium tracking-widest text-white">MODERN FURNITURE COLLECTION</p>
             <h2 className='text-5xl lg:text-9xl text-white font-thin mt-2 lg:mt-3'>Premium Italian</h2>
-            <button className="text-black bg-white py-3 px-7 border-0 text-sm mt-5 lg:mt-3 ">CHECK INVENTORY</button>
+            <button className="text-black bg-white py-3 px-7 border-2 text-sm mt-5 lg:mt-3 hover:bg-transparent hover:border-2 hover:text-white transition-all">CHECK INVENTORY</button>
      </div>
     </div>
   );
